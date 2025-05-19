ac4fb603e1079d6d0b1b8d864cf35160cc3fe8d3
admin <luhieutrung2602@gmail.com>
2025-05-19 13:30:57 +0700

    trung

--- QLTB/src/main/java/com/nvd/controllers/ApiIssueControllerss.java
+++ QLTB/src/main/java/com/nvd/controllers/ApiIssueControllerss.java
@@ -6,8 +6,12 @@ package com.nvd.controllers;
 
 import com.nvd.pojo.IncidentLevel;
 import com.nvd.pojo.Issue;
+import com.nvd.pojo.Repair;
 import com.nvd.service.IncidentLevelService;
 import com.nvd.service.IssueService;
+import com.nvd.service.RepairService;
+import com.nvd.service.RepairTypeService;
+import java.math.BigDecimal;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,6 +24,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +41,10 @@ public class ApiIssueControllerss {
     private IssueService issueService;
     @Autowired
     private IncidentLevelService incidentLevelService;
+    @Autowired
+    private RepairService repairService;
+    @Autowired
+    private RepairTypeService repairTypeService;
 
     @DeleteMapping("/issue/{id}/delete")
     @ResponseStatus(HttpStatus.NO_CONTENT)
@@ -64,12 +73,19 @@ public class ApiIssueControllerss {
     }
 
     @PostMapping("/issue/{id}/confirm")
-    public ResponseEntity<Issue> edit(@PathVariable(value = "id") int id) {
+    public ResponseEntity<Issue> edit(@PathVariable(value = "id") int id,
+            @RequestParam("cost") BigDecimal cost,
+            @RequestParam("accountId") int accountId) {
         Issue p = this.issueService.getIssueById(id);
         if (p == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-
+        if (p != null && !p.getIsResolved()) {
+            p.setIsResolved(true);
+            issueService.addOrUpdateIssue(p);
+            Repair repair = new Repair();
+            repairService.addNewMaintenancyOrIssue(repair, cost, p.getDeviceId(), this.repairTypeService.getTypeById(2), accountId);
+        }
         p.setIsResolved(true);
         return new ResponseEntity<>(this.issueService.addOrUpdateIssue(p), HttpStatus.OK);
     }
